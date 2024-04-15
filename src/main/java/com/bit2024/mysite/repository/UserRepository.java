1cbae66dc1289f94e211734e96661f9b7e8201b7
BIT <BIT@DESKTOP-V9BD38C>
2024-04-15 15:08:07 +0900

    datasource

--- src/main/java/com/bit2024/mysite/repository/UserRepository.java
+++ src/main/java/com/bit2024/mysite/repository/UserRepository.java
@@ -6,25 +6,25 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.bit2024.mysite.vo.UserVo;
 
 @Repository
 public class UserRepository {
-
+	@Autowired
+	private DataSource dataSource;
+	
 	public Boolean insert(UserVo vo) {
 		Boolean result = false;
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		
 		try {
-			// 0. JDBC Driver Loading
-			Class.forName("org.mariadb.jdbc.Driver");
-
-			// 1. 연결: (서버IP+port+dbname) 연결URL, 사용자아이디, 비밀번호
-			String url = "jdbc:mariadb://192.168.0.112:3307/webdb?chatset=utf8";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
+			conn = dataSource.getConnection();
 			
 			// 2. sql 준비
 			String sql = "insert into user values(null, ?, ?, password(?), ?)";
@@ -40,8 +40,6 @@ public class UserRepository {
 			int count = pstmt.executeUpdate();
 			
 			result = count == 1;			
-		} catch (ClassNotFoundException e) {
-			System.out.println("Driver Loading Fail:" + e);
 		} catch (SQLException e) {
 			System.out.println("Error:" + e);
 		} finally {
@@ -67,12 +65,7 @@ public class UserRepository {
 		ResultSet rs = null;
 		
 		try {
-			// 0. JDBC Driver Loading
-			Class.forName("org.mariadb.jdbc.Driver");
-
-			// 1. 연결: (서버IP+port+dbname) 연결URL, 사용자아이디, 비밀번호
-			String url = "jdbc:mariadb://192.168.0.112:3307/webdb?chatset=utf8";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
+			conn = dataSource.getConnection();
 			
 			// 2. sql 준비
 			String sql = "select no, name from user where email=? and password=password(?)";
@@ -94,8 +87,6 @@ public class UserRepository {
 				result.setNo(no);
 				result.setName(name);
 			}
-		} catch (ClassNotFoundException e) {
-			System.out.println("Driver Loading Fail:" + e);
 		} catch (SQLException e) {
 			System.out.println("Error:" + e);
 		} finally {
